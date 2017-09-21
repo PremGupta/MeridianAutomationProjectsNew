e6f7d494f3c02a74042638a1968983dda952038b
Prem Gupta <prem2604@gmail.com>
2017-09-22 02:55:32 +0530

    First Commit

---
+++ MeridianAutoProjectNew/src/main/java/ObjectRepository/AwardsCentralHomePage.java
@@ -0,0 +1,37 @@
+package ObjectRepository;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
+public class AwardsCentralHomePage{
+	
+	public static WebDriver driver;
+
+	By shopbydepartment =By.xpath("//a[text()='Shop by Department ']");
+	By accessories =By.xpath("//a[text()='Accessories']");
+	By blenders =By.xpath("//a[text()='Blenders']");
+	
+	   
+	public AwardsCentralHomePage(WebDriver driver)
+	{
+		this.driver=driver;
+	}
+	
+	   public  WebElement ShopByDepartment()
+	   {
+		   return driver.findElement(shopbydepartment);
+	   }
+	   
+	   public  WebElement Accessories()
+	   {
+		   return driver.findElement(accessories);
+	   }
+	   
+	   public  WebElement Blenders()
+	   {
+		   return driver.findElement(blenders);
+	   }
+   
+}
